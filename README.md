0481f7ed68925d4cbfb3a987d618d7ac8bd039a3
Jasper Robison <jasper.robison22@gmail.com>
2022-02-25 14:59:41 +1100

    add BridgeSwitch handling to KafkaBean

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# serviceq Project
+# serviceq
 
 A Natively Compiled Genny Service initiazation package, used to build other Genny services.
 
--- src/main/java/life/genny/serviceq/intf/KafkaBean.java
+++ src/main/java/life/genny/serviceq/intf/KafkaBean.java
@@ -4,8 +4,16 @@ import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 
+import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecordMetadata;
+import org.eclipse.microprofile.reactive.messaging.Message;
+
+import life.genny.qwandaq.data.BridgeSwitch;
 import life.genny.qwandaq.intf.KafkaInterface;
+import life.genny.qwandaq.models.GennyToken;
 import life.genny.serviceq.live.data.InternalProducer;
 
 @ApplicationScoped
@@ -14,7 +22,9 @@ public class KafkaBean implements KafkaInterface {
 	@Inject 
 	InternalProducer producer;
 
-	private static final Logger log = Logger.getLogger(KafkaBean.class);
+	static final Logger log = Logger.getLogger(KafkaBean.class);
+
+    static Jsonb jsonb = JsonbBuilder.create();
 
 	/**
 	* Write a string payload to a kafka channel.
@@ -24,17 +34,14 @@ public class KafkaBean implements KafkaInterface {
 	 */
 	public void write(String channel, String payload) { 
 
+		JsonObject payloadObj = jsonb.fromJson(payload, JsonObject.class);
+		GennyToken userToken = new GennyToken(payloadObj.getString("token"));
+
 		if ("data".equals(channel)) {
 			producer.getToData().send(payload);
 
 		} else if ("valid_data".equals(channel)) {
 			producer.getToValidData().send(payload);
-
-		} else if ("webcmds".equals(channel)) {
-			producer.getToWebCmds().send(payload);
-
-		} else if ("webdata".equals(channel)) {
-			producer.getToWebData().send(payload);
 
 		} else if ("search_events".equals(channel)) {
 			producer.getToSearchEvents().send(payload);
@@ -47,6 +54,28 @@ public class KafkaBean implements KafkaInterface {
 
 		} else if ("schedule".equals(channel)) {
 			producer.getToSchedule().send(payload);
+
+		} else if ("webcmds".equals(channel)) {
+
+			String bridgeId = BridgeSwitch.bridges.get(userToken.getUniqueId());
+
+			OutgoingKafkaRecordMetadata<String> metadata = OutgoingKafkaRecordMetadata.<String>builder()
+				.withTopic(bridgeId + "-" + channel)
+				.build();
+
+			producer.getToWebCmds().send(Message.of(payloadObj.toString()).addMetadata(metadata));
+
+		} else if ("webdata".equals(channel)) {
+
+			producer.getToWebData().send(payload);
+
+			String bridgeId = BridgeSwitch.bridges.get(userToken.getUniqueId());
+
+			OutgoingKafkaRecordMetadata<String> metadata = OutgoingKafkaRecordMetadata.<String>builder()
+				.withTopic(bridgeId + "-" + channel)
+				.build();
+
+			producer.getToWebCmds().send(Message.of(payloadObj.toString()).addMetadata(metadata));
 
 		} else {
 			log.error("Producer unable to write to channel " + channel);
